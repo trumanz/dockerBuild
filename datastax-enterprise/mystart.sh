d4f4d6ad86592420e0ad279ca3fe2671e492a535
Truman Zhou <truman.ck.zhou@gmail.com>
2015-12-03 23:35:29 +0800

    improve

--- datastax-enterprise/mystart.sh
+++ datastax-enterprise/mystart.sh
@@ -2,18 +2,18 @@
 start_nodes(){
    for n in $(echo "node0 node1 node2 node3 node4 node5 node6 node7 node8"); do 
      echo $n
-     docker run --name=$n  -h $n -t -d -i  test2/datastax:latest  /bin/bash
+     docker run --name=$n  -h $n -t -d -i   test/datastax   /bin/bash
    done
 }
 
-IP0=$(docker inspect --format='{{ .NetworkSettings.IPAddress }}'  node0)
-IP3=$(docker inspect --format='{{ .NetworkSettings.IPAddress }}'  node3)
-echo "node0 IP $IP0"
-echo "node3 IP $IP3"
 
 
 
 config_node(){
+IP0=$(docker inspect --format='{{ .NetworkSettings.IPAddress }}'  node0)
+IP3=$(docker inspect --format='{{ .NetworkSettings.IPAddress }}'  node3)
+echo "node0 IP $IP0"
+echo "node3 IP $IP3"
    for n in  $(echo "node0 node1 node2 node3 node4 node5 node6 node7 node8"); do 
      echo $n
      docker cp  dse-entrypoint-config $n:/
@@ -34,11 +34,22 @@ start_service() {
    DSE_HOME=/opt/dse
    #config_node
    #start  seed first, node0-2 as cassandra node
-   docker exec  -d -t -i  node0   sh -c "gosu dse $DSE_HOME/bin/dse cassandra  -f"    #cassandra node
+   docker exec  -d -t -i  node0   sh -c "gosu dse $DSE_HOME/bin/dse cassandra  -f >/dse.log  2>&1"    #cassandra node
+   sleep 10
    #docker exec  -d -t -i  node3   gosu dse $DSE_HOME/bin/dse cassandra  -f  -s -k #Search Analytics node,  spark + solr
-   docker exec  -d -t -i  node1   sh -c "gosu dse $DSE_HOME/bin/dse cassandra  -f"   
-   docker exec  -d -t -i  node2   sh -c "gosu dse $DSE_HOME/bin/dse cassandra  -f"  
+   docker exec  -d -t -i  node1   sh -c "gosu dse $DSE_HOME/bin/dse cassandra  -f >/dse.log  2>&1"   
+   sleep 10
+   docker exec  -d -t -i  node2   sh -c "gosu dse $DSE_HOME/bin/dse cassandra  -f >/dse.log  2>&1"  
+   sleep 10
 
-   docker exec  -d -t -i  node6   sh -c  "gosu dse $DSE_HOME/bin/dse cassandra  -f  -s"  #search node , solr
-   docker exec  -d -t -i  node7   sh -c  "gosu dse $DSE_HOME/bin/dse cassandra  -f  -s" 
+   docker exec  -d -t -i  node6   sh -c  "gosu dse $DSE_HOME/bin/dse cassandra  -f  -s >/dse.log  2>&1"  #search node , solr
+   sleep 10
+   docker exec  -d -t -i  node7   sh -c  "gosu dse $DSE_HOME/bin/dse cassandra  -f  -s >/dse.log  2>&1" 
+   sleep 10
 }
+
+
+#start_nodes
+config_node
+#start_service
+
